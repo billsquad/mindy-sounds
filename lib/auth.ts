6e2dc5a5c224ea74aa14a28c5ba70b2bc5418191
Adrian Zinko <adrian.zinko@billennium.com>
2021-12-14 18:27:17 +0100

    middleware validation

---
+++ lib/auth.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { MINDY_ACCESS_TOKEN: token } = req.cookies;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verify(token, "secret");
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error("User does not exist");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "Unauthorized" });
+        return;
+      }
+      return handler(req, res, user);
+    }
+
+    res.status(401);
+    res.json({ error: "Unauthorized" });
+  };
+};
